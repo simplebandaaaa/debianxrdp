8753d48c7d304eafdc62716c0da96ed395a8e5ef
simplebandaaaa <simplebandaa@outlook.com>
2026-07-10 17:00:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# किसी भी पुराने बचे हुए सॉकेट या PID फाइलों को डिलीट करना (ताकि रीस्टार्ट में एरर न आए)
+# XRDP के लिए ज़रूरी रनटाइम डायरेक्टरी बनाना (यह सबसे ज़रूरी है)
+mkdir -p /var/run/xrdp
+mkdir -p /var/run/dbus
+
+# किसी भी पुराने बचे हुए सॉकेट या PID फाइलों को डिलीट करना
 rm -f /var/run/xrdp/xrdp.pid
 rm -f /var/run/xrdp/xrdp-sesman.pid
 rm -f /var/run/xrdp/xrdp_sesman.socket
 
 # D-Bus और मशीन आईडी को सही से कॉन्फ़िगर और स्टार्ट करना
-mkdir -p /var/run/dbus
 dbus-uuidgen --ensure
 dbus-daemon --system --fork
 
-# XRDP सेशन मैनेजर (sesman) को स्टार्ट करना
+# XRDP सेशन मैनेजर (sesman) को बैकग्राउंड में स्टार्ट करना
 xrdp-sesman --nodaemon &
 
-# थोड़ा रुकना ताकि sesman पूरी तरह चालू हो जाए
+# थोड़ा रुकना ताकि sesman पूरी तरह एक्टिव हो जाए
 sleep 2
 
 # मुख्य XRDP सर्वर को फोरग्राउंड में चलाना ताकि कंटेनर बंद न हो
