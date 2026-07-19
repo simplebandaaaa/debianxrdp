f2e221119b179c02c8df5f2f6a17c70eb22e83bf
simplebandaaaa <simplebandaa@outlook.com>
2026-07-19 21:04:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
-# पुरानी लॉक फाइल्स हटाना
+# पुरानी बची-कुची लॉक फाइल्स को पूरी तरह हटाना
 rm -rf /var/run/xrdp/* /var/run/dbus/* /tmp/.X11-unix/X*
 
-# DBus और दोनों सर्विसेस को सीधे बाइनरी से चलाना बिना किसी 'service start' के
+# DBus और XRDP को सीधे बाइनरी से शुरू करना (बिना किसी 'service start' के)
 dbus-daemon --system --fork
-
-# xrdp-sesman को बैकग्राउंड में चलाना
 xrdp-sesman
-
-# xrdp को फ़ोरग्राउंड में चलाना ताकि कंटेनर चालू रहे
-echo "RDP running..."
 exec xrdp --nodaemon
