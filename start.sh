2aef93c9969e0df527817e900e7273824ae564a8
simplebandaaaa <simplebandaa@outlook.com>
2026-07-19 20:05:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-if [ -z "$CHROME_CODE" ]; then
-    echo "ERROR: CHROME_CODE वेरिएबल खाली है। कृपया नया कोड डालें।"
-    exit 1
-fi
-
-# लाइव कोड के साथ होस्ट शुरू करना
-DISPLAY= /opt/google/chrome-remote-desktop/start-host \
-    --code="$CHROME_CODE" \
-    --redirect-url="https://remotedesktop.google.com/_/oauthredirect" \
-    --name="Chromebook-Docker" \
-    --pin="123456"
-
-tail -f /dev/null
+service dbus start
+service xrdp start
+exec xrdp-sesman --nodaemon
